df534f9a6fa6bc4804341cd5c1dfeb7eb543511f
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-05-16 10:18:12 +0000

    会員新規登録画面view

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -59,7 +59,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
-  
+
   def after_sign_up_path_for(resource) #会員のサインアップ後の遷移先
     my_page_path(current_customer.id)
   end
