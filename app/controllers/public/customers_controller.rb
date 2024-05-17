faa6d1e9a532f96e7e1564000af6ac0e73202913
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-05-17 02:13:54 +0000

    customersコントローラアクション作成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,11 +1,36 @@
 class Public::CustomersController < ApplicationController
   # before_action :authenticate_customer! 後ほど有効にしてください。
+  # before_action :ensure_current_customer! 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
+  end
+  
+  def update
+    customer = Customer.find(params[:id])
+    customer.update(params_customer)
   end
 
   def unsubscribe
   end
+  
+  def withdraw
+    current_customer.update(is_active: false
+    redirect_to new_customer_session_path
+  end
+  
+  private
+  
+  def params_customer #developマージしたらkaneからkanaへなおす
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kane, :post_code, :address, :telephone_number, :email)
+  end
+  
+  def ensure_current_customer
+    customer = Customer.find(params[:id])
+    unless current_customer.id == customer.id
+      redirect_to my_page_path(current_customer.id)
+    end
 end
