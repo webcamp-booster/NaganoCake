42387bdb8bd9954c775bd21095c8b48ee261b044
hyohyo2 <hono.handball.0910@gmail.com>
2024-05-19 09:49:10 +0000

    商品ページレイアウト修正等

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,7 +1,7 @@
 class Admin::ItemsController < ApplicationController
-  # before_action :authenticate_admin!　後ほど有効にしてください。
+  before_action :authenticate_admin!
   def index
-    @items = Item.page(params[:page]).per(1)
+    @items = Item.page(params[:page]).per(8)
   end
 
   def new
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,12 +1,13 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item.page(params[:page]).per(8)
+    # ページネーションの対象を販売ステータスのtrueに絞る、ページネーションの設定
+    @items = Item.where(is_active: true).page(params[:page]).per(8)
     @active_items_count = Item.where(is_active: true).count
   end
 
   def show
     @item = Item.find(params[:id])
-    @cart_item =CartItem
+    @cart_item = CartItem
   end
   
   
