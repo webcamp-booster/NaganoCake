fefdc5d8f3e0f214c380dfef1a5d22e1aa06dce5
hyohyo2 <hono.handball.0910@gmail.com>
2024-05-19 05:25:12 +0000

    カート：コントローラ設定・修正

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -4,11 +4,37 @@ class Public::CartItemsController < ApplicationController
   def index
     @cart_items = current_customer.cart_items
   end
+  
+  def create
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.amount += params[:cart_item][:quantity].to_i
+      
+      cart_item.save
+      redirect_to cart_items_path
+    elsif @cart_item.save
+          @cart_items = current_customer.cart_items.all
+          render :index
+    else
+          render :index
+    end
+  end
+  
+  def update
+    @cart_item = CartItem.find(params[:id])
+    if @cart_item.update(cart_item_params)
+      redirect_to cart_items_path
+    else
+      @cart_items = current_customer.cart_items
+      render :index
+    end
+  end
 
   def destroy
     cart_item = CartItem.find(params[:id])
     cart_item.destroy
-    @cartitems = CartItem.all
+    @cart_items = CartItem.all
     render 'index'
   end
 
@@ -17,6 +43,9 @@ class Public::CartItemsController < ApplicationController
     render 'index'
   end
 
-
+private
+ def cart_item_params
+   params.require(:cart_item).permit(:item_id, :price, :amount)
+ end
 
 end
