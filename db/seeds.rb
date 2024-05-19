ee1aba0cea27c1e57f50994d395f23d0334af3b4
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-05-19 09:07:29 +0000

    seed内のインデント修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,23 +13,23 @@ Admin.find_or_create_by(id: 1) do |admin|
 end
   #本番環境時は.env利用と、gitignoreへの記載要!!
 
-  Customer.create!(
-   email: 'test@test.com',
-   last_name: 'テスト',
-   first_name:'太郎',
-   last_name_kana: 'テスト',
-   first_name_kana: 'タロウ',
-   password: 'example',
-   post_code: '1111111',
-   address: 'n県n市',
-   telephone_number: '0120333906',
-   is_active: true
+Customer.create!(
+  email: 'test@test.com',
+  last_name: 'テスト',
+  first_name:'太郎',
+  last_name_kana: 'テスト',
+  first_name_kana: 'タロウ',
+  password: 'example',
+  post_code: '1111111',
+  address: 'n県n市',
+  telephone_number: '0120333906',
+  is_active: true
 )
 
 Customer.find_by(email: 'test@test.com').addresses.create!(
-    name: 'テスト花子',
-    post_code: '1234567',
-    address: 'a県b市'
+  name: 'テスト花子',
+  post_code: '1234567',
+  address: 'a県b市'
   )
 
 Genre.create!(
@@ -38,14 +38,14 @@ Genre.create!(
 
 # 退会済の顧客データ
 Customer.create!(
-   email: 'test2@test.com',
-   last_name: 'テスト2',
-   first_name: '小太郎',
-   last_name_kana: 'テスト2',
-   first_name_kana: 'コタロウ',
-   password: 'example2',
-   post_code: '2222222',
-   address: 's県s市',
-   telephone_number: '0120222222',
-   is_active: false
+  email: 'test2@test.com',
+  last_name: 'テスト2',
+  first_name: '小太郎',
+  last_name_kana: 'テスト2',
+  first_name_kana: 'コタロウ',
+  password: 'example2',
+  post_code: '2222222',
+  address: 's県s市',
+  telephone_number: '0120222222',
+  is_active: false
 )
