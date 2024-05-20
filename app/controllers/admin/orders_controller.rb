e0da872f019c6bf6f017ee3a9213bd0d61921c7e
Hiroki Koga <164776044+jusn4@users.noreply.github.com>
2024-05-20 11:16:30 +0900

    Update orders_controller.rb
    order_itemをorder_detailに変更

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -15,25 +15,25 @@ def show
 	# 注文内容の情報を取得しています！
 	@order = Order.find(params[:id])
   # 注文内容の商品を取得しています！
-	@order_items = @order.order_items
+	@order_details = @order.order_idetails
 end
 
 def update
 	@order = Order.find(params[:id]) #注文詳細の特定
-	@order_items = @order.order_items #注文から紐付く商品の取得
+	@order_details = @order.order_details #注文から紐付く商品の取得
 	@order.update(order_params) #注文ステータスの更新
 
 
  if @order.status == "入金確認" #注文ステータスが入金確認なら下の事をする
-     @order_items.update_all(making_status: 1) #製作ステータスを「製作待ちに」　更新
+     @order_details.update_all(making_status: 1) #製作ステータスを「製作待ちに」　更新
  end
 		 redirect_to  admin_order_path(@order) #注文詳細に遷移
 end
   # こっちでも可能
   # emunの変換前取得している
 	# if @order.read_attribute_before_type_cast(:order_status) == 1
-	# 	@order_items.each do |order_item|
-	# 	order_item.update(making_status: 1)
+	# 	@order_details.each do |order_detail|
+	# 	order_detail.update(making_status: 1)
 	# 	end
  #  end
 
