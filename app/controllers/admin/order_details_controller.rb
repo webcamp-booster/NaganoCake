0739b7c210450acf00f6dd3eac5aa39079106e1c
jusna <164776044+jusn4@users.noreply.github.com>
2024-05-22 08:22:45 +0000

    テストで引っかかったところ修正

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,3 +1,16 @@
 class Admin::OrderDetailsController < ApplicationController
   before_action :authenticate_admin!
+  def update
+    #@order = Order.find(params[:order_id])
+    @order_detail = OrderDetail.find(params[:id])
+    @order_detail.update(order_detail_params)
+    
+    
+    redirect_to  admin_order_path(@order_detail.order_id)
+  end 
+  
+  private
+ def order_detail_params
+   params.require(:order_detail).permit(:making_status)
+ end
 end
