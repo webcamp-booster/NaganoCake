5b7d411bdbfbd921425cf81e9659f58217030308
jusna <164776044+jusn4@users.noreply.github.com>
2024-05-22 10:06:44 +0000

    ページネーションの追加

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,11 +1,11 @@
 class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
   def top
-    @orders = Order.all
+    @orders = Order.all.order(created_at: :desc).page(params[:page]).per(10)
   end
-  
+
   def customer_orders
     @customer = Customer.find(params[:id])
-    @orders = @customer.orders
+    @orders = @customer.orders.order(created_at: :desc).page(params[:page]).per(10)
   end
 end
