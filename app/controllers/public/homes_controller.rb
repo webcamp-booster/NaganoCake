8e94268c0429d6f1b53a7c032cba469c95366980
hyohyo2 <hono.handball.0910@gmail.com>
2024-05-22 09:08:42 +0000

    コメントアウト変更

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
     @genres = Genre.all 
-    @items = Item.limit(4)# 先頭から5つのレコードから８つを取得
+    @items = Item.limit(4)# 先頭から4つのレコードを取得
   end
 
   def about
