e335a3e44e93e4c8371808d73aea6b5c7bcffa94
hyohyo2 <hono.handball.0910@gmail.com>
2024-05-23 02:43:44 +0000

    トップページ商品一覧編集

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
     @genres = Genre.all 
-    @items = Item.order('id DESC').limit(4)# 先頭から4つのレコードを取得
+    @items = Item.where(is_active: true).order('id DESC').limit(4)# 先頭から4つのレコードを取得
   end
 
   def about
